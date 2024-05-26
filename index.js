d933f5722df6c58644c6b4e29f96e6b05be11925
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-26 11:35:36 -0400

    pumpkin's meme picker
    -import the data

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/*
+Challenge:
+1. Move the data to a file called data.js 
+   and import it into index.js.
+*/
+ 
+
 const catsData = [
     {
         emotionTags: ["moody"],
@@ -181,17 +188,6 @@ function getEmotionsArray(cats){
     return emotionsArray
 }
 
-/*
-Challenge:
-1. Take control of the 'emotion-radios' div.
-2. In renderEmotionsRadios, set up a let 
-   to hold our string of HTML. You can initialise
-   it with an empty string.
-3. Iterate over "emotions" and put each emotion 
-   in a <p> tag and then add them to the let you 
-   created in step 2. 
-4. Render the string to the 'emotion-radios' div.
-*/ 
 
 function renderEmotionsRadios(cats){
     let radioItems = ``
